b55be5558cc269e409ef6f3b8963e9662f26b495
SHYNTA <shynta.add@gmail.com>
2017-03-16 14:31:42 +0700

    report fix

--- public/resources/views/layout/sidebar.blade.php
+++ public/resources/views/layout/sidebar.blade.php
@@ -34,7 +34,7 @@
 							</li>
 						</ul>
 					</li>
-					<li><a><i class="fa fa-edit"></i> Inventory <span class="fa fa-chevron-down"></span></a>
+					<li><a><i class="fa fa-cubes"></i> Inventory <span class="fa fa-chevron-down"></span></a>
 						<ul class="nav child_menu" style="display: none">
 							<li><a href="summary">Summary</a>
 							</li>
@@ -44,29 +44,9 @@
 							</li>
 						</ul>
 					</li>
-					<li><a href="report"><i class="fa fa-desktop"></i> Reports <span class="fa fa-chevron-down"></span></a>
-						<ul class="nav child_menu" style="display: none">
-							<li><a href="general_elements.html">General Elements</a>
-							</li>
-							<li><a href="media_gallery.html">Media Gallery</a>
-							</li>
-							<li><a href="typography.html">Typography</a>
-							</li>
-							<li><a href="icons.html">Icons</a>
-							</li>
-							<li><a href="glyphicons.html">Glyphicons</a>
-							</li>
-							<li><a href="widgets.html">Widgets</a>
-							</li>
-							<li><a href="invoice.html">Invoice</a>
-							</li>
-							<li><a href="inbox.html">Inbox</a>
-							</li>
-							<li><a href="calender.html">Calender</a>
-							</li>
-						</ul>
+					<li><a href="report"><i class="fa fa-file-text"></i> Reports </a>
 					</li>
-					<li><a><i class="fa fa-table"></i> Customers <span class="fa fa-chevron-down"></span></a>
+					<li><a><i class="fa fa-users"></i> Customers <span class="fa fa-chevron-down"></span></a>
 						<ul class="nav child_menu" style="display: none">
 							<li><a href="customerlist">Customers List</a>
 							</li>
@@ -74,55 +54,13 @@
 							</li>
 						</ul>
 					</li>
-					<li><a><i class="fa fa-bar-chart-o"></i> Library <span class="fa fa-chevron-down"></span></a>
+					<li><a><i class="fa fa-list"></i> Library <span class="fa fa-chevron-down"></span></a>
 						<ul class="nav child_menu" style="display: none">
 							<li><a href="product">Product</a>
 							</li>
 							<li><a href="discount">Discount</a>
 							</li>
-							<li><a href="morisjs.html">Moris JS</a>
-							</li>
-							<li><a href="echarts.html">ECharts </a>
-							</li>
-							<li><a href="other_charts.html">Other Charts </a>
-							</li>
 						</ul>
-					</li>
-				</ul>
-			</div>
-			<div class="menu_section">
-				<h3>Live On</h3>
-				<ul class="nav side-menu">
-					<li><a><i class="fa fa-bug"></i> Additional Pages <span class="fa fa-chevron-down"></span></a>
-						<ul class="nav child_menu" style="display: none">
-							<li><a href="e_commerce.html">E-commerce</a>
-							</li>
-							<li><a href="projects.html">Projects</a>
-							</li>
-							<li><a href="project_detail.html">Project Detail</a>
-							</li>
-							<li><a href="contacts.html">Contacts</a>
-							</li>
-							<li><a href="profile.html">Profile</a>
-							</li>
-						</ul>
-					</li>
-					<li><a><i class="fa fa-windows"></i> Extras <span class="fa fa-chevron-down"></span></a>
-						<ul class="nav child_menu" style="display: none">
-							<li><a href="page_404.html">404 Error</a>
-							</li>
-							<li><a href="page_500.html">500 Error</a>
-							</li>
-							<li><a href="plain_page.html">Plain Page</a>
-							</li>
-							<li><a href="login.html">Login Page</a>
-							</li>
-							<li><a href="pricing_tables.html">Pricing Tables</a>
-							</li>
-
-						</ul>
-					</li>
-					<li><a><i class="fa fa-laptop"></i> Landing Page <span class="label label-success pull-right">Coming Soon</span></a>
 					</li>
 				</ul>
 			</div>
--- public/resources/views/report.blade.php
+++ public/resources/views/report.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.header')
+@extends('layout.datatable')
 @section('title')
 Report
 @stop
@@ -38,43 +38,481 @@ Report
         <!-- required for floating -->
         <!-- Nav tabs -->
         <ul class="nav nav-tabs tabs-left">
-          <li class="active"><a href="#home" data-toggle="tab">Sales Summary</a>
+          <li class="active"><a href="#sales" data-toggle="tab">Sales Summary</a>
           </li>
-          <li><a href="#profile" data-toggle="tab">Payment Methods</a>
+          <li><a href="#payment" data-toggle="tab">Payment Methods</a>
           </li>
-          <li><a href="#messages" data-toggle="tab">Item Sales</a>
+          <li><a href="#item" data-toggle="tab">Item Sales</a>
           </li>
-          <li><a href="#settings" data-toggle="tab">Category Sales</a>
+          <li><a href="#category" data-toggle="tab">Category Sales</a>
           </li>
-          <li><a href="#settings" data-toggle="tab">Modifier Sales</a>
+          <li><a href="#modifier" data-toggle="tab">Modifier Sales</a>
           </li>
-          <li><a href="#settings" data-toggle="tab">Discounts</a>
+          <li><a href="#discount" data-toggle="tab">Discounts</a>
           </li>
-          <li><a href="#settings" data-toggle="tab">Taxes</a>
+          <li><a href="#taxes" data-toggle="tab">Taxes</a>
           </li>
-          <li><a href="#settings" data-toggle="tab">Gratuity</a>
+          <li><a href="#gratuity" data-toggle="tab">Gratuity</a>
           </li>
-          <li><a href="#settings" data-toggle="tab">Collected By</a>
+          <li><a href="#collected" data-toggle="tab">Collected By</a>
           </li>
-          <li><a href="#settings" data-toggle="tab">Served By</a>
+          <li><a href="#served" data-toggle="tab">Served By</a>
           </li>
         </ul>
       </div>
 
       <div class="col-xs-9">
         <!-- Tab panes -->
-        <div class="tab-content">
-          <div class="tab-pane active" id="home">
-            <p class="lead">Home tab</p>
-            <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher
-              synth. Cosby sweater eu banh mi, qui irure terr.</p>
+        <div class="tab-content x_panel">
+          <div class="tab-pane active" id="sales">
+            <table class="tile_custom dt-responsive nowrap" id="datatable-buttons">
+              <thead>
+                <tr class="tabhead">
+                  <td>Type</td>
+                  <td>Amount</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Gross Sales</td>
+                  <td>Rp 1.814.930</td>
+                </tr>
+                <tr>
+                  <td>Discount</td>
+                  <td>(Rp 13)</td>
+                </tr>
+                <tr>
+                  <td>Refund</td>
+                  <td>(Rp 0)</td>
+                </tr>
+                <tr class="line">
+                  <td >Net Sales</td>
+                  <td >Rp 1.814.917</td>
+                </tr>
+                <tr>
+                  <td>Gratuity</td>
+                  <td>Rp 0</td>
+                </tr>
+                <tr>
+                  <td>Tax</td>
+                  <td>Rp 0</td>
+                </tr>
+                <tr class="total">
+                  <td>Total Collected</td>
+                  <td>Rp 1.814.917</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="tab-pane" id="payment">
+            <table class="tile_custom table table-striped dt-responsive nowrap" id="datatable-payment">
+              <thead>
+                <tr>
+                  <td>Paymnet Method</td>
+                  <td>Number Of Transaction</td>
+                  <td>Total Collected</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Cash</td>
+                  <td>0</td>
+                  <td>Rp 0</td>
+                </tr>
+                <tr>
+                  <td>Debit/ Credit</td>
+                  <td>0</td>
+                  <td>Rp 0</td>
+                </tr>
+                <tr>
+                  <td>Invoice</td>
+                  <td>1</td>
+                  <td>Rp 500</td>
+                </tr>
+                <tr>
+                  <td>EDC</td>
+                  <td>6</td>
+                  <td>Rp 1.214.417</td>
+                </tr>
+                <tr>
+                  <td>Gift Card</td>
+                  <td>0</td>
+                  <td>Rp 0</td>
+                </tr>
+                <tr>
+                  <td>Other</td>
+                  <td>2</td>
+                  <td>Rp 600.000</td>
+                </tr>
+                <tr class="total">
+                  <td>Total</td>
+                  <td>9</td>
+                  <td>Rp 1.814.917</td>
+                </tr>
+              </tbody>
+            </table></div>
+            <div class="tab-pane" id="item">
+              <table class="tile_custom table table-striped dt-responsive nowrap" id="datatable-item">
+                <thead>
+                  <tr>
+                    <td>Name</td>
+                    <td>SKU</td>
+                    <td>Category</td>
+                    <td>Item Sold</td>
+                    <td>Gross Sales</td>
+                    <td>Discount</td>
+                    <td>Refund</td>
+                    <td>Net Sales</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Custom Amout</td>
+                    <td></td>
+                    <td>Uncategorized</td>
+                    <td>5</td>
+                    <td>Rp 14.930</td>
+                    <td>(Rp 13)</td>
+                    <td></td>
+                    <td>Rp 14.917</td>
+                  </tr>
+                  <tr>
+                    <td>Samsung</td>
+                    <td></td>
+                    <td>Uncategorized</td>
+                    <td>6</td>
+                    <td>Rp 1.800.000</td>
+                    <td></td>
+                    <td></td>
+                    <td>Rp 1.800.000</td>
+                  </tr>
+                  <tr class="total">
+                    <td>Total</td>
+                    <td></td>
+                    <td></td>
+                    <td>11</td>
+                    <td>Rp 1.814.930</td>
+                    <td>(Rp 13)</td>
+                    <td></td>
+                    <td>Rp 1.814.917</td>
+                  </tr>
+                </tbody>
+              </table></div>
+              <div class="tab-pane" id="category"><table class="tile_custom table table-striped dt-responsive nowrap" id="datatable-category">
+                <thead>
+                  <tr>
+                    <td>Category</td>
+                    <td>Item Sold</td>
+                    <td>Gross Sales</td>
+                    <td>Discount</td>
+                    <td>Refund</td>
+                    <td>Net Sales</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Uncategorized</td>
+                    <td>11</td>
+                    <td>Rp 1.814.930</td>
+                    <td>(Rp 13)</td>
+                    <td></td>
+                    <td>Rp 1.814.930</td>
+                  </tr>
+                  <tr class="total">
+                    <td>Total</td>
+                    <td>11</td>
+                    <td>Rp 1.814.930</td>
+                    <td>(Rp 13)</td>
+                    <td></td>
+                    <td>Rp 1.814.930</td>
+                  </tr>
+                </tbody>
+              </table></div>
+              <div class="tab-pane" id="modifier"><table class="tile_custom table table-striped dt-responsive nowrap" id="datatable-modifier">
+                <thead>
+                  <tr>
+                    <td>Name</td>
+                    <td>Quantity Sold</td>
+                    <td>Gross Sales</td>
+                    <td>Discount</td>
+                    <td>Refund</td>
+                    <td>Net Sales</td>
+                  </tr>
+                </thead>
+                <tbody>
+                </tbody>
+              </table></div>
+              <div class="tab-pane" id="discount"><table class="tile_custom table table-striped dt-responsive nowrap" id="datatable-discount">
+                <thead>
+                  <tr>
+                    <td>Name</td>
+                    <td>Discount Amount</td>
+                    <td>Count</td>
+                    <td>Gross Discount</td>
+                    <td>Discount Refunded</td>
+                    <td>Net Discount</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>teh</td>
+                    <td>2%</td>
+                    <td>2</td>
+                    <td>(Rp 13)</td>
+                    <td></td>
+                    <td>(Rp 13)</td>
+                  </tr>
+                  <tr class="total">
+                    <td>Total</td>
+                    <td>2%</td>
+                    <td>2</td>
+                    <td>(Rp 13)</td>
+                    <td></td>
+                    <td>(Rp 13)</td>
+                  </tr>
+                </tbody>
+              </table></div>
+              <div class="tab-pane" id="taxes"><table class="tile_custom table table-striped dt-responsive nowrap" id="datatable-taxes">
+                <thead>
+                  <tr>
+                    <td>Name</td>
+                    <td>Tax Rate</td>
+                    <td>Taxable Amount</td>
+                    <td>Tax Collected</td>
+                  </tr>
+                </thead>
+                <tbody>
+                </tbody>
+              </table></div>
+              <div class="tab-pane" id="gratuity"><table class="tile_custom table table-striped dt-responsive nowrap" id="datatable-gratuity">
+                <thead>
+                  <tr>
+                    <td>Name</td>
+                    <td>Rate</td>
+                    <td>Gratuity Collected</td>
+                  </tr>
+                </thead>
+                <tbody>
+                </tbody>
+              </table></div>
+              <div class="tab-pane" id="served"><table class="tile_custom table table-striped dt-responsive nowrap" id="datatable-served">
+                <thead>
+                  <tr>
+                    <td>Name</td>
+                    <td>Title</td>
+                    <td>Number of Transactions</td>
+                    <td>Total Collected</td>
+                  </tr>
+                </thead>
+                <tbody>
+                </tbody>
+              </table></div>
+              <div class="tab-pane" id="collected"><table class="tile_custom table table-striped dt-responsive nowrap" id="datatable-collected">
+                <thead>
+                  <tr>
+                    <td>Name</td>
+                    <td>Title</td>
+                    <td>Number of Transactions</td>
+                    <td>Total Collected</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>denykalpar</td>
+                    <td>Owner</td>
+                    <td>9</td>
+                    <td>Rp 1.814.917</td>
+                  </tr>
+                  <tr class="total">
+                    <td>Total</td>
+                    <td></td>
+                    <td>9</td>
+                    <td>Rp 1.814.917</td>
+                  </tr>
+                </tbody>
+              </table></div>
             </div>
-            <div class="tab-pane" id="profile">Profile Tab.</div>
-            <div class="tab-pane" id="messages">Messages Tab.</div>
-            <div class="tab-pane" id="settings">Settings Tab.</div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-  @stop
+    @stop
+    @section('js')
+    <script>
+      $(document).ready(function() {
+        $('#datatable-buttons').DataTable( {
+          "searching": false,
+          "paging":   false,
+          "ordering": false,
+          "info":     false,
+          dom: 'Bfrtip',
+          buttons: [{
+            extend: "csv",
+            className: "btn-sm"
+          }, {
+            extend: "excel",
+            className: "btn-sm"
+          }, {
+            extend: "print",
+            className: "btn-sm"
+          }]
+        } );
+
+        $('#datatable-payment').DataTable( {
+          "searching": false,
+          "paging":   false,
+          "ordering": false,
+          "info":     false,
+          dom: 'Bfrtip',
+          buttons: [{
+            extend: "csv",
+            className: "btn-sm"
+          }, {
+            extend: "excel",
+            className: "btn-sm"
+          }, {
+            extend: "print",
+            className: "btn-sm"
+          }]
+        } );
+
+        $('#datatable-item').DataTable( {
+          "searching": false,
+          "paging":   false,
+          "ordering": false,
+          "info":     false,
+          dom: 'Bfrtip',
+          buttons: [{
+            extend: "csv",
+            className: "btn-sm"
+          }, {
+            extend: "excel",
+            className: "btn-sm"
+          }, {
+            extend: "print",
+            className: "btn-sm"
+          }]
+        } );
+
+        $('#datatable-category').DataTable( {
+          "searching": false,
+          "paging":   false,
+          "ordering": false,
+          "info":     false,
+          dom: 'Bfrtip',
+          buttons: [{
+            extend: "csv",
+            className: "btn-sm"
+          }, {
+            extend: "excel",
+            className: "btn-sm"
+          }, {
+            extend: "print",
+            className: "btn-sm"
+          }]
+        } );
+        $('#datatable-modifier').DataTable( {
+          "searching": false,
+          "paging":   false,
+          "ordering": false,
+          "info": false,
+          dom: 'Bfrtip',
+          buttons: [{
+            extend: "csv",
+            className: "btn-sm"
+          }, {
+            extend: "excel",
+            className: "btn-sm"
+          }, {
+            extend: "print",
+            className: "btn-sm"
+          }]
+        } );
+        $('#datatable-discount').DataTable( {
+          "searching": false,
+          "paging":   false,
+          "ordering": false,
+          "info":     false,
+          dom: 'Bfrtip',
+          buttons: [{
+            extend: "csv",
+            className: "btn-sm"
+          }, {
+            extend: "excel",
+            className: "btn-sm"
+          }, {
+            extend: "print",
+            className: "btn-sm"
+          }]
+        } );
+        $('#datatable-taxes').DataTable( {
+          "searching": false,
+          "paging":   false,
+          "ordering": false,
+          "info": false,
+          dom: 'Bfrtip',
+          buttons: [{
+            extend: "csv",
+            className: "btn-sm"
+          }, {
+            extend: "excel",
+            className: "btn-sm"
+          }, {
+            extend: "print",
+            className: "btn-sm"
+          }]
+        } );
+        $('#datatable-gratuity').DataTable( {
+          "searching": false,
+          "paging":   false,
+          "ordering": false,
+          "info": false,
+          dom: 'Bfrtip',
+          buttons: [{
+            extend: "csv",
+            className: "btn-sm"
+          }, {
+            extend: "excel",
+            className: "btn-sm"
+          }, {
+            extend: "print",
+            className: "btn-sm"
+          }]
+        } );
+        $('#datatable-collected').DataTable( {
+          "searching": false,
+          "paging":   false,
+          "ordering": false,
+          "info": false,
+          dom: 'Bfrtip',
+          buttons: [{
+            extend: "csv",
+            className: "btn-sm"
+          }, {
+            extend: "excel",
+            className: "btn-sm"
+          }, {
+            extend: "print",
+            className: "btn-sm"
+          }]
+        } );
+        $('#datatable-served').DataTable( {
+          "searching": false,
+          "paging":   false,
+          "ordering": false,
+          "info": false,
+          dom: 'Bfrtip',
+          buttons: [{
+            extend: "csv",
+            className: "btn-sm"
+          }, {
+            extend: "excel",
+            className: "btn-sm"
+          }, {
+            extend: "print",
+            className: "btn-sm"
+          }]
+        } );
+      } );
+    </script>
+    @stop
